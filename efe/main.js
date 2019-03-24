a57a50839306a0a3f13ac8983562a719801691dd
Raul Arrabales <raul.arrabales@gmail.com>
2019-03-24 18:36:37 +0100

    experiment data loading

--- efe/main.js
+++ efe/main.js
@@ -1,16 +1,35 @@
 
+// Using dataframes
+var DataFrame = dfjs.DataFrame;
+
+// Loading experiment design data
+DataFrame.fromCSV('http://psicobotica.com/prolexitim/efe/efe-experiment-1.csv').then(loadExperiment);
+
+// Experiment data
+var series = []; 
+var trials = [];
+var imagefiles = [];
+
+var curSerie = 0; 
+var curTrial = 0;
+var curImageFile = ""; 
+
+
 // Finite State Machine definition for EFE Test 
  var machine = new StateMachine({
     init: 'start',
     transitions: [
-      { name: 'imgs_loaded',     from: 'start',  to: 'welcome' },
+	  { name: 'experiment_loaded',     from: 'start',  to: 'images' },
+      { name: 'images_loaded',     from: 'images',  to: 'welcome' },
       { name: 'got_new_email',   from: 'welcome', to: 'demographics'  },
 	  { name: 'got_known_email',   from: 'welcome', to: 'instructions'  },
       { name: 'got_demographics', from: 'demographics', to: 'instructions'    },
-      { name: 'acknowledge', from: 'instructions',    to: 'fixation' },
+      { name: 'acknowledge', from: 'instructions',    to: 'series' },
+	  { name: 'init_new_series', from: 'series',    to: 'fixation' },
 	  { name: 'fixation_timeout', from: 'fixation',    to: 'efe_challenge' },
 	  { name: 'response', from: 'efe_challenge',    to: 'fixation' },
-	  { name: 'finish', from: 'efe_challenge',    to: 'end' }
+	  { name: 'finish_serie', from: 'efe_challenge',    to: 'series' },
+	  { name: 'finish_experiment', from: 'series',    to: 'end' }
     ],
 	data: {
 		current_iteration: 1,
@@ -19,7 +38,8 @@
     methods: {
 	  getCurIter: function() { return this.current_iteration },
 	  getCurImg: function() { return thiscurrent_img },
-      onImgsLoaded:     function() { console.log('Images loaded')    },
+	  onExperimentLoaded:     function() { console.log('Experiment loaded')    },
+      onImgagesLoaded:     function() { console.log('Images loaded')    },
       onGotNewEmail:   function() { console.log('New user, need demographics')     },
 	  onGotKnowmEmail:   function() { console.log('Known user, retrieved demographics')     },
       onGotDemographics: function() { console.log('Demographic data recorded') },
@@ -31,7 +51,7 @@
   });
   
  // AVAILABLE TRANSITION FUNCTIONS
- // machine.imgs_loaded() 
+ // machine.imgsLoaded() 
  // machine.got_new_email() 
  // machine.got_known_email() 
  // machine.got_demographics()
@@ -47,4 +67,23 @@
  // machine.transitions()
  
  
- 
+ function loadExperiment(df)
+ {
+	console.log("experiment data is ready."); 
+	// console.log(df.show()); 
+	series = df.select("series").toArray();
+	trials = df.select("trial").toArray();
+	imagefiles = df.select("image").toArray();
+	
+	// transition the machine
+	machine.experimentLoaded();
+	
+	document.getElementById("status").style.visibility = "visible";
+	document.getElementById("status_body").innerHTML = 
+		"Estado: " + machine.state + ". Cargado: " + series.length + " series y " + trials.length + " ensayos.";
+	console.log(series.toString());
+	console.log(trials.toString());
+	console.log(imagefiles.toString());
+	
+	
+ }
